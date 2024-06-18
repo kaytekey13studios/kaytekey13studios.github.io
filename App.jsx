97a3db1c09711def08342996ba884857b5452c96
kaytekey13studios <kayte@key13studios.com>
2024-06-18 15:14:38 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+// component.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return Hello world;
+  }
+};
+
+ReactDOM.render(, document.querySelector('#container'));
